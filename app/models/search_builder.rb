6e52f0098df9ecedd19818114451d4a916e50786
Ray Lubinsky <rwl@virginia.edu>
2018-10-02 17:21:22 -0400

    Blacklight installation
    (cherry picked from commit a4cecc94757f0ca9c33b96d449ec8aa98909de84)

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -1,30 +1,12 @@
-# app/models/blacklight/search_builder.rb
-#
 # frozen_string_literal: true
-# warn_indent:           true
-
-__loading_begin(__FILE__)
-
-require 'blacklight/search_builder'
-require 'blacklight/lens/search_builder_behavior'
-
-# SearchBuilder
-#
-# @example Adding a new step to the processor chain
-#   self.default_processor_chain += [:add_custom_data_to_query]
-#
-#   def add_custom_data_to_query(solr_parameters)
-#     solr_parameters[:custom] = blacklight_params[:user_value]
-#   end
-#
-# @see Blacklight::SearchBuilder
-# @see Blacklight::Lens::SearchBuilderBehavior
-#
 class SearchBuilder < Blacklight::SearchBuilder
-  include Blacklight::Lens::SearchBuilderBehavior
-  include BlacklightAdvancedSearch::AdvancedSearchBuilder
-  self.default_processor_chain += [:add_advanced_parse_q_to_solr, :add_advanced_search_to_solr]
+  include Blacklight::Solr::SearchBuilderBehavior
+
+  ##
+  # @example Adding a new step to the processor chain
+  #   self.default_processor_chain += [:add_custom_data_to_query]
+  #
+  #   def add_custom_data_to_query(solr_parameters)
+  #     solr_parameters[:custom] = blacklight_params[:user_value]
+  #   end
 end
-
-__loading_end(__FILE__)
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,78 +1,16 @@
-# app/models/user.rb
-#
-# frozen_string_literal: true
-# warn_indent:           true
-
-__loading_begin(__FILE__)
-
-# User
-#
-# @see Blacklight::User
-# @see Devise::Models
-#
 class User < ApplicationRecord
 
-  # Connects this user object to bookmarks and searches.
+  # Connects this user object to Blacklights Bookmarks.
   include Blacklight::User
-
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
 
-  # ===========================================================================
-  # :section: Blacklight::User overrides
-  # ===========================================================================
-
-  public
-
-  # Overrides the Blacklight method which arbitrarily uses the document type
-  # of the first of *docs* as a database query criterion.  This method just
-  # uses the document IDs for the database query.
-  #
-  # @param [Array<Blacklight::Document>, nil] docs
-  #
-  # @return [Array<ActiveRecord>]
-  #
-  # This method overrides
-  # @see Blacklight::User#bookmarks_for_documents
-  #
-  def bookmarks_for_documents(docs = nil)
-    bookmarks_for(docs)
-  end
-
-  # ===========================================================================
-  # :section: Object overrides
-  # ===========================================================================
-
-  public
-
-  # Method added by Blacklight; Blacklight uses #to_s on your user class to get
-  # a user-displayable login/identifier for the account.
-  #
-  # @return [String]
-  #
+  # Method added by Blacklight; Blacklight uses #to_s on your
+  # user class to get a user-displayable login/identifier for
+  # the account.
   def to_s
     email
   end
-
-  # ===========================================================================
-  # :section:
-  # ===========================================================================
-
-  public
-
-  # Return the user's bookmarks for the given item(s).
-  #
-  # @param [Array<Blacklight::Document>] docs
-  #
-  # @return [Array<ActiveRecord>]
-  #
-  def bookmarks_for(*docs)
-    ids = docs.flatten.compact.map(&:id).presence
-    ids ? bookmarks.where(document_id: ids) : []
-  end
-
 end
-
-__loading_end(__FILE__)
