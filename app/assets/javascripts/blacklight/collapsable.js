7ee51dcbb74064edfabe3b44a1d29738a0df27b2
Ray Lubinsky <rwl@virginia.edu>
2019-03-04 15:56:37 -0500

    [VIRGONEW-566] Update suggester to use the new _suggest fields - Modified app/assets/javascripts/blacklight/collapsable.js so that the click handler is overridden for href="" as well as href="#".

--- app/assets/javascripts/blacklight/collapsable.js
+++ app/assets/javascripts/blacklight/collapsable.js
@@ -1,15 +1,26 @@
 // app/assets/javascripts/blacklight/collapsable.js
 //
-// This code is essentially unchanged from the original Blacklight source.
+// This code has been modified from the original Blacklight source in order to
+// allow click handler overrides for href="" as well as href="#".
+//
 // @see https://github.com/projectblacklight/blacklight/blob/v7.0.1/app/javascript/blacklight/collapsable.js
 
 (function($) {
     Blacklight.onLoad(function() {
+
+        var COLLAPSIBLE_LINKS = [
+            'a[data-toggle=collapse][href="#"]',
+            'a[data-toggle=collapse][href=""]',
+            '[data-toggle=collapse] a[href="#"]',
+            '[data-toggle=collapse] a[href=""]'
+        ].join(', ');
+
         // When clicking on a link that toggles the collapsing behavior, don't
         // do anything with the hash or the page could jump around.
         $(document).on('click',
-            'a[data-toggle=collapse][href="#"], [data-toggle=collapse] a[href="#"]',
+            COLLAPSIBLE_LINKS,
             function(event) { event.preventDefault(); }
         );
+
     });
 })(jQuery);
